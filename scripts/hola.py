31fa064b9a75f7e70490a1552d9f86023499f5a1
abrahamfigueroagonzalezgonzalez009 -afk <abraham.figueroa.gonzalez009@gmail.com>
2026-01-13 15:42:08 -0700

    escribir edad y nombre

--- scripts/hola.py
+++ scripts/hola.py
@@ -1,9 +1,6 @@
-print ("resultado")
-
-numero = 0
-numero = 3
-
-numeroSuma = 6
-
-numeroResultado = numero + numeroSuma
-print(numeroResultado)
+#elaborar un programa en phyiton que le pida al el usuario 2 numeror y de el resultado.
+print("como te llamas")
+nombre=input()
+print("cual es tu edad")
+edad=input()
+print(f"hola, {nombre} , tu edad es {edad} ")
